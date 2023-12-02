b51f135da769ba3bce0287ef0237a57ec074d3f6
kamck <kamck@users.noreply.github.com>
2023-12-01 18:06:42 -0600

    Clean up day 1 part 2

--- 01/b.rb
+++ 01/b.rb
@@ -12,20 +12,22 @@ def find_all_occurrences(str, search)
   str.enum_for(:scan, search).map { Regexp.last_match.begin(0) }
 end
 
-def find_indexes(line)
-  all_indexes = DIGITS.each_with_index.flat_map do |digit, index|
+def find_numeric_words(line)
+  DIGITS.each_with_index.flat_map do |digit, index|
     find_all_occurrences(line, digit).map { Index.new(_1, index + 1) }
   end
-
-  line.chars.each_with_index do |char, i|
-    all_indexes << Index.new(i, char.to_i) if char =~ /[[:digit:]]/
-  end
-
-  all_indexes
 end
 
-d = ARGF.each_line.map do |line|
-  indexes = find_indexes(line.chomp)
+def find_digits(line)
+  line.chars
+    .each_with_index
+    .select { |char, _| char =~ /[[:digit:]]/ }
+    .map { |char, i| Index.new(i, char.to_i) }
+end
+
+d = ARGF.map do |line|
+  indexes = find_numeric_words(line) + find_digits(line)
   "#{indexes.min.value}#{indexes.max.value}".to_i
 end
+
 p d.sum
