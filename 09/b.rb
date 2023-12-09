b032509e7b4d38efd0c703374f211f22d4f5e2ed
kamck <kamck@users.noreply.github.com>
2023-12-09 09:58:38 -0600

    Added day 9

---
+++ 09/b.rb
@@ -0,0 +1,15 @@
+def extrapolate_next(data)
+  data.reverse.inject(0) {|sum, line| line.first - sum }
+end
+
+next_data = ARGF.map do |line|
+  data = [line.chomp.split.map(&:to_i)]
+
+  loop do
+    new_data = data.last.each_cons(2).map { |(e1, e2)| e2 - e1 }
+    break extrapolate_next(data) if new_data.all?(&:zero?)
+    data.push(new_data)
+  end
+end
+
+p next_data.sum
