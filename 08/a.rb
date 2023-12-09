d6a2650239f61b43f729592a11093f78f32faf45
kamck <kamck@users.noreply.github.com>
2023-12-09 10:05:09 -0600

    Clean up day 8

--- 08/a.rb
+++ 08/a.rb
@@ -1,23 +1,20 @@
 Pair = Data.define(:L, :R)
 
+steps = ARGF.readline.chomp.chars
+ARGF.readline                        # Throw away
+
 map = ARGF.each_with_object({}).each_with_index do |(line, obj), index|
-  if index == 0
-    obj[:steps] = line.chomp.chars
-  elsif index == 1
-  else
-    node, next_steps = line.chomp.split(/\s=\s/)
-    obj[node] = Pair.new(*next_steps.gsub(/\(/, "").gsub(/\)/, "").split(", "))
-  end
+  node, next_steps = line.chomp.split(/\s=\s/)
+  obj[node] = Pair.new(*next_steps.gsub(/\(/, "").gsub(/\)/, "").split(", "))
 end
 
 step_count = 0
 curr_node = "AAA"
 
 loop do
-  step_no = step_count % map[:steps].length
-  next_step = map[:steps][step_no]
+  next_step = steps[step_count % steps.length].to_sym
 
-  curr_node = map[curr_node].send(next_step.to_sym)
+  curr_node = map[curr_node].send(next_step)
   step_count += 1
 
   break if curr_node == "ZZZ"
--- 08/b.rb
+++ 08/b.rb
@@ -1,24 +1,20 @@
 Pair = Data.define(:L, :R)
 
-map = ARGF.each_with_object({}).each_with_index do |(line, obj), index|
-  if index == 0
-    obj[:steps] = line.chomp.chars
-  elsif index == 1
-  else
-    node, next_steps = line.chomp.split(/\s=\s/)
-    obj[node] = Pair.new(*next_steps.gsub(/\(/, "").gsub(/\)/, "").split(", "))
-  end
-end
+steps = ARGF.readline.chomp.chars
+ARGF.readline                        # Throw away
 
+map = ARGF.each_with_object({}).each_with_index do |(line, obj), index|
+  node, next_steps = line.chomp.split(/\s=\s/)
+  obj[node] = Pair.new(*next_steps.gsub(/\(/, "").gsub(/\)/, "").split(", "))
+end
 
 steps = map.keys.select { _1.end_with? "A" }.collect do |curr_node|
   step_count = 0
 
   loop do
-    step_no = step_count % map[:steps].length
-    next_step = map[:steps][step_no]
+    next_step = steps[step_count % steps.length].to_sym
 
-    curr_node = map[curr_node].send(next_step.to_sym)
+    curr_node = map[curr_node].send(next_step)
     step_count += 1
 
     break if curr_node.end_with? "Z"
